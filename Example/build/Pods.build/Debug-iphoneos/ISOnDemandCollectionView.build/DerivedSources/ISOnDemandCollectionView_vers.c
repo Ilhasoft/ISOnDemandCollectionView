177f7b643e70b2d09f5a23150899bb77cd27c64b
Yves <raphael.bastos@me.com>
2017-08-17 14:48:33 -0300

    fixed untracked files

--- Example/build/Pods.build/Debug-iphoneos/ISOnDemandCollectionView.build/DerivedSources/ISOnDemandCollectionView_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char ISOnDemandCollectionViewVersionString[];
- extern const double ISOnDemandCollectionViewVersionNumber;
-
- const unsigned char ISOnDemandCollectionViewVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:ISOnDemandCollectionView  PROJECT:Pods-1" "\n";
- const double ISOnDemandCollectionViewVersionNumber __attribute__ ((used)) = (double)1.;
--- Example/build/Pods.build/Debug-iphoneos/Pods-ISOnDemandCollectionView_Example.build/DerivedSources/Pods_ISOnDemandCollectionView_Example_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char Pods_ISOnDemandCollectionView_ExampleVersionString[];
- extern const double Pods_ISOnDemandCollectionView_ExampleVersionNumber;
-
- const unsigned char Pods_ISOnDemandCollectionView_ExampleVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_ISOnDemandCollectionView_Example  PROJECT:Pods-1" "\n";
- const double Pods_ISOnDemandCollectionView_ExampleVersionNumber __attribute__ ((used)) = (double)1.;
